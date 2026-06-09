c67a01f4345d20669ee049f734c9eee14a00a2b4
EloiFournier05 <eloifour@gmail.com>
2026-06-09 15:02:19 +0200

    Load homepage cover CSS fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { GameSessionProvider } from "./session/GameSessionProvider";
 import "./index.css";
+import "./homeCover.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
